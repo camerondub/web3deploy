d1858dfa6b34a1c96003105ac5d3bde96a1b0f7e
Cameron Wong <cw@reosource.io>
2021-11-17 03:54:18 +0000

    Update format for external install_requires dep

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages=find_packages(where="src"),
     install_requires=[
         "python-decouple",
-        "rlog @ https://github.com/camerondub/python-rlog/archive/master.zip#egg=rlog-0.0.1",
+        "rlog @ git+https://github.com/camerondub/python-rlog.git#egg=rlog",
         "py-solc-x",
         "web3",
     ],
