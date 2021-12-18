91b4469b53a14ceb3807021f221f00374a0cae8a
Cameron Wong <cw@reosource.io>
2021-12-18 22:40:51 +0000

    Ensure WEB3_POA env is properly cast to bool

--- src/web3deploy/__init__.py
+++ src/web3deploy/__init__.py
@@ -121,7 +121,7 @@ def deploy():
             Web3.HTTPProvider(config("WEB3_HTTP_PROVIDER", default="http://localhost:8545"))
         )
 
-        if config("WEB3_POA", default=False):
+        if config("WEB3_POA", default=False, cast=bool):
             rlog.info("Injecting geth_poa_middleware")
             w3.middleware_onion.inject(geth_poa_middleware, layer=0)
         w3.eth.default_account = w3.eth.accounts[config("WEB3_KEY_INDEX", cast=int, default=0)]
